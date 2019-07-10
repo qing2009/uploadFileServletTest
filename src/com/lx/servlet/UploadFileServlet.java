820359d6f23b504600fea036b97c858c963525dd
liuxin <471823471@qq.com>
2019-07-10 18:31:10 +0800

    first commit

---
+++ src/com/lx/servlet/UploadFileServlet.java
@@ -0,0 +1,105 @@
+package com.lx.servlet;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Iterator;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ * Servlet implementation class UploadFileServlet
+ */
+@WebServlet("/UploadFileServlet")
+@MultipartConfig
+public class UploadFileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UploadFileServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		doGet(request, response);
+		
+//		Enumeration<String> headerNameEnum = request.getHeaderNames();
+//		while (headerNameEnum.hasMoreElements()) {
+//			String key = headerNameEnum.nextElement();
+//			String vlaue = request.getHeader(key);
+//			System.out.println(key+":"+vlaue);
+//		}
+		
+		Part part = request.getPart("file");
+//		Collection<String> headerNames = part.getHeaderNames();
+//		Iterator<String> it = headerNames.iterator();
+//		while (it.hasNext()) {
+//			String key = it.next();
+//			String value = part.getHeader(key);
+//			System.out.println(key+":"+value);
+//			
+//		}
+		
+		String fileName = "temp";
+		String contentDisposition = part.getHeader("content-disposition");
+		if(contentDisposition!=null) {
+			int startIndex = contentDisposition.indexOf("filename=\"")+"filename=\"".length();
+			
+			if(startIndex>=0) {
+				fileName = contentDisposition.substring(startIndex,contentDisposition.length()-1);
+			}
+		}
+		
+		String filePath = this.getServletContext().getRealPath("/upload");
+		
+		System.out.println("filePath:"+filePath);
+		System.out.println("fileName:"+fileName);
+		
+		InputStream iStream = part.getInputStream();
+		FileOutputStream fStream = new FileOutputStream(new File(filePath,fileName));
+		
+		int uploadedLength = 0;
+		int len = 0;
+		byte[] b = new byte[1024*1024];
+		while((len=iStream.read(b)) !=-1) {
+			fStream.write(b,0,len);
+			uploadedLength = uploadedLength+len;
+		}
+		
+		fStream.close();
+		iStream.close();
+		part.delete();
+	}
+	
+	/**
+	 * 断点续传分析：
+	 * 1、记录上传文件的已上传进度。
+	 * 	每次写入成功，都记录已上传的中字节数。
+	 * 2、根据进度读取文件并完成上传。
+	 */
+
+}
